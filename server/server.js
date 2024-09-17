0d8a188e137524b86ab373b2de0150d3c190bfa2
Milan Egri <117757061+MilanEgri@users.noreply.github.com>
2024-09-17 20:32:16 +0200

    can get restaruant by id route

--- server/server.js
+++ server/server.js
@@ -10,13 +10,30 @@ const restaruantData = require("./restaruant_dummy_data.json");
 const app = express();
 app.use(express.json());
 
-app.get("/restaruants", async (req, res) => {
+app.get("/restaurants", async (req, res) => {
   try {
-    const restaruants = restaruantData
-    if (!restaruants) {
-      return res.status(501).json("restarusants not exist");
+    const restaurants = restaruantData
+    if (!restaurants) {
+      return res.status(501).json("restaurants not exist");
     } 
-    return res.status(200).json(restaruants);
+    return res.status(200).json(restaurants);
+  } catch (err) {
+    return res.status(500).json(err.message);
+  }
+});
+app.get("/restaurant/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const restaurants = restaruantData
+    const restaurant = restaurants.find(e => e.id == id);
+
+    if (restaurant) {
+      return res.status(200).json(restaurant);
+    } else {
+      return res.status(404).json("Restaurant not found");
+    }
+
   } catch (err) {
     return res.status(500).json(err.message);
   }
