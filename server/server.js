357de321ba837f21cf95213347d1828d6563ac55
Milan Egri <117757061+MilanEgri@users.noreply.github.com>
2024-09-17 19:41:59 +0200

    restaruant get route working

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const PORT = 8000;
+const restaruantData = require("./restaruant_dummy_data.json");
 ///const { MONGO_URL } = process.env;
 
 /*if (!MONGO_URL) {
@@ -8,6 +9,18 @@ const PORT = 8000;
   }*/
 const app = express();
 app.use(express.json());
+
+app.get("/restaruants", async (req, res) => {
+  try {
+    const restaruants = restaruantData
+    if (!restaruants) {
+      return res.status(501).json("restarusants not exist");
+    } 
+    return res.status(200).json(restaruants);
+  } catch (err) {
+    return res.status(500).json(err.message);
+  }
+});
 
 const main = async () => {
     //await mongoose.connect(MONGO_URL);
