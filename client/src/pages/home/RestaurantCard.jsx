1c6cc1c9baea71f94194a0b21746a500c849de23
Dornfeld Zoltán <dornfeldz@gmail.com>
2024-09-18 20:29:54 +0200

    refactoring restaurant slide

---
+++ client/src/pages/home/RestaurantCard.jsx
@@ -0,0 +1,19 @@
+function RestaurantCard({ restaurant }) {
+  return (
+    <div key={restaurant.id} className="bg-[#fae5d3] p-4 rounded-lg shadow-md">
+      <h2 className="text-xl font-bold">Restaurant {restaurant.id}</h2>
+      <p className="text-[#888]">
+        <strong>Telephone:</strong> {restaurant.telephone_number}
+      </p>
+      <p className="text-[#888]">
+        <strong>Email:</strong> {restaurant.email_address}
+      </p>
+      <p className="text-[#888]">
+        <strong>Coordinates:</strong> {restaurant.coordinates.latitude},{" "}
+        {restaurant.coordinates.longitude}
+      </p>
+    </div>
+  );
+}
+
+export default RestaurantCard;
--- client/src/pages/home/RestaurantsSlide.jsx
+++ client/src/pages/home/RestaurantsSlide.jsx
@@ -1,4 +1,5 @@
 import data from "../../../../server/restaruant_dummy_data.json";
+import RestaurantCard from "./RestaurantCard";
 
 function RestaurantsSlide({ title }) {
   return (
@@ -7,22 +8,7 @@ function RestaurantsSlide({ title }) {
       <div className="overflow-x-scroll custom-scrollbar">
         <div className="flex gap-4 mt-6 mb-2">
           {data.map((restaurant) => (
-            <div
-              key={restaurant.id}
-              className="bg-[#fae5d3] p-4 rounded-lg shadow-md"
-            >
-              <h2 className="text-xl font-bold">Restaurant {restaurant.id}</h2>
-              <p className="text-[#888]">
-                <strong>Telephone:</strong> {restaurant.telephone_number}
-              </p>
-              <p className="text-[#888]">
-                <strong>Email:</strong> {restaurant.email_address}
-              </p>
-              <p className="text-[#888]">
-                <strong>Coordinates:</strong> {restaurant.coordinates.latitude},{" "}
-                {restaurant.coordinates.longitude}
-              </p>
-            </div>
+            <RestaurantCard key={restaurant.id} restaurant={restaurant} />
           ))}
         </div>
       </div>
