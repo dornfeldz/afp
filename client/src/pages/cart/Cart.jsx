2c30f54d1adc0d430f3cffaff8b80db02670486a
Dornfeld Zoltán <dornfeldz@gmail.com>
2024-09-18 22:58:31 +0200

    styling cart

--- client/src/pages/cart/Cart.jsx
+++ client/src/pages/cart/Cart.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { HiOutlineXMark } from "react-icons/hi2";
 
 const CartModal = ({ isOpen, onClose, cartItems }) => {
   return (
@@ -7,18 +8,23 @@ const CartModal = ({ isOpen, onClose, cartItems }) => {
         isOpen ? "translate-x-0" : "translate-x-full"
       }`}
     >
-      <div className="p-4">
-        <button className="text-xl font-bold" onClick={onClose}>
-          ×
+      <div className="flex flex-col justify-between h-full px-4">
+        <div>
+          <button className="h-16 mb-10 text-3xl font-bold" onClick={onClose}>
+            <HiOutlineXMark />
+          </button>
+          <h2 className="mb-4 text-2xl font-semibold">Your Order</h2>
+          <ul>
+            {cartItems.map((item, index) => (
+              <li key={index} className="mb-2">
+                {item.name} - ${item.price}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <button className="mt-8 px-6 py-3 bg-[#e67e22] text-white rounded-full font-semibold shadow hover:bg-[#cf711f] transition-colors duration-200">
+          Check out
         </button>
-        <h2 className="mb-4 text-2xl font-semibold">Your Cart</h2>
-        <ul>
-          {cartItems.map((item, index) => (
-            <li key={index} className="mb-2">
-              {item.name} - ${item.price}
-            </li>
-          ))}
-        </ul>
       </div>
     </div>
   );
