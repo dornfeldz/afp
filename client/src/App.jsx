dfbeef8194b7f8855b14d9d00274341f76cd4dcb
Haskó Sándor <hasko.sandor01@gmail.com>
2024-09-18 18:30:22 +0200

    Added schemas to server

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+import Cart from "./pages/cart/Cart";
+import Home from "./pages/home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
