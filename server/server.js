861746d2331d176ad9e53aab93fbeb98860804b8
SHasko17 <138365503+SHasko17@users.noreply.github.com>
2024-10-10 20:12:27 +0200

    new file containing routes to database

--- server/server.js
+++ server/server.js
@@ -1,23 +1,87 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const PORT = 8000;
-const restaruantData = require("./restaruant_dummy_data.json");
-const produtctDatat = require("./item_dummy_data.json");
+//const restaruantData = require("./restaruant_dummy_data.json");
+//const produtctDatat = require("./item_dummy_data.json");
+
+require("dotenv").config();
+const { MONGO_URL } = process.env;
+
+if (!MONGO_URL) {
+  console.error("Missing MONGO_URL environment variable");
+  process.exit(1);
+}
+//Setting up the database
+// Adding schemas start ----------------------------------
+const restaurantSchema = new mongoose.Schema({
+  name: String,
+  bio: String,
+  profilePicture: String,
+  cover: String,
+  address: String,
+  telephone: [Number],
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  adress: {
+    street: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    zipcode: {
+        type: Number,
+        required: true
+    }
+},
+location: {
+  type: {
+      type: String,
+      enum: ['Point'],
+      default: 'Point'
+  },
+  coordinates: {
+      type: [Number],
+      required: true
+  } 
+
+ },
+
+  menu: [],
+});
+
+const menuSchema = new mongoose.Schema({
+
+  restaurant: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "Restaurant"
+  },
+
+  menu: {
+      type: String,
+      required: true
+  } ,
+  price: {
+      type: Number,
+      required: true
+  }
+})
+
+//Adding the schemas end ----------------------------------
 
 const app = express();
+
 app.use(express.json());
 
 app.get("/restaurants", async (req, res) => {
-  try {
-    const restaurants = restaruantData
-    if (!restaurants) {
-      return res.status(501).json("restaurants not exist");
-    } 
-    return res.status(200).json(restaurants);
-  } catch (err) {
-    return res.status(500).json(err.message);
-  }
+ return res.status(200).json("restaurants");
 });
-app.get("/restaurant/:id", async (req, res) => {
+/*app.get("/restaurant/:id", async (req, res) => {
   const id = req.params.id;
 
   try {
@@ -76,16 +140,17 @@ app.post("/product", async (req, res) => {
     console.log(err);
     return res.status(500).json(err.message);
   }
-});
+});*/
 
 const main = async () => {
+  await mongoose.connect(MONGO_URL);
 
-    app.listen(PORT, () => {
-      console.log(`server is running on port ${PORT}`);
-    });
-  };
-  
-  main().catch((err) => {
-    console.error(err);
-    process.exit(1);
+  app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
   });
+};
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
