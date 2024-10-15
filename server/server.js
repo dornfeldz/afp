dbd3e00a96bee792688d978a7d93d038f3b91b0a
Milan Egri <117757061+MilanEgri@users.noreply.github.com>
2024-10-15 21:05:10 +0200

    delete helper functions

--- server/server.js
+++ server/server.js
@@ -39,66 +39,7 @@ app.get("/restaurant/:id", async (req, res) => {
     return res.status(500).json(err.message);
   }
 });
-/*app.get("/restaurant/:id", async (req, res) => {
-  const id = req.params.id;
 
-  try {
-    const restaurants = restaruantData
-    const restaurant = restaurants.find(e => e.id == id);
-
-    if (restaurant) {
-      return res.status(200).json(restaurant);
-    } else {
-      return res.status(404).json("Restaurant not found");
-    }
-
-  } catch (err) {
-    return res.status(500).json(err.message);
-  }
-});
-app.get("/product/:id", async (req, res) => {
-  const id = req.params.id;
-
-  try {
-    const products = produtctDatat
-    const product = products.find(e => e.id == id);
-
-    if (product) {
-      return res.status(200).json(product);
-    } else {
-      return res.status(404).json("Product not found");
-    }
-
-  } catch (err) {
-    return res.status(500).json(err.message);
-  }
-});
-app.post("/restaurant", async (req, res) => {
-  let restaurant = req.body;
-  try {
-    console.log("restaruant posted")
-    console.log(restaurant)
-    let saved = {}
-    saved.id = 99
-    return res.status(201).json(saved.id);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err.message);
-  }
-});
-app.post("/product", async (req, res) => {
-  let product = req.body;
-  try {
-    console.log("product posted")
-    console.log(product)
-    let saved = {}
-    saved.id = 99
-    return res.status(201).json(saved.id);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err.message);
-  }
-});*/
 
 const main = async () => {
   await mongoose.connect(MONGO_URL);
