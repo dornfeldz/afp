e4a94004fe4991eeeaa16378cd95629ed7f37a38
Dornfeld Zoltán <dornfeldz@gmail.com>
2024-10-21 20:22:32 +0200

    responsive header

--- client/src/ui/Header.jsx
+++ client/src/ui/Header.jsx
@@ -15,7 +15,7 @@ function Header() {
   return (
     <div className="w-full flex items-center justify-between h-16 px-4 shadow-[0px_2px_4px_rgba(0,0,0,0.1)]">
       <Link to="/">
-        <h1 className="text-xl font-extrabold">
+        <h1 className="text-2xl font-extrabold md:text-4xl">
           Tasty<span className="text-[#e67e22]">Go</span>
         </h1>
       </Link>
@@ -25,11 +25,11 @@ function Header() {
           name="main"
           id="main"
           placeholder="Search for a restaurant..."
-          className="bg-[#fae5d3] text-[#888] py-1 px-4 rounded-l-full focus:outline-[#888] hidden"
+          className="bg-[#fae5d3] text-[#888] py-1 px-4 rounded-l-full focus:outline-[#888] hidden md:block"
         />
         <button
           type="submit"
-          className="bg-[#e67e22] py-2 px-3 rounded-r-full text-[#fdf2e9]"
+          className="bg-[#e67e22] hidden md:block py-2 px-3 rounded-r-full text-[#fdf2e9]"
         >
           <HiMagnifyingGlass className="text-[#fae5d3]" />
         </button>
