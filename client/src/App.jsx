a8e2ca70ad38d36f65bd1efdf4905a8f1eaf03a5
Juhász Péter <juhszp@gmail.com>
2025-01-02 22:22:31 +0100

    javítás

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import {
 } from "react-router-dom";
 import AppLayout from "./ui/AppLayout";
 import Home from "./pages/home/Home";
-import CartModal from "./ui/Cart";
 import Restaurant from "./pages/restaurant/Restaurant";
 import Contact from "./pages/footer pages/contact/Contact";
 import TermsAndConditions from "./pages/footer pages/terms-and-conditions/TermsAndConditions";
@@ -17,7 +16,7 @@ import Profile from "./pages/profile/Profile";
 import Checkout from "./pages/checkout/Checkout";
 import Login from "./pages/login/Login";
 import Order from "./pages/order/Order";
-import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
+import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
 
 const router = createBrowserRouter([
   {
@@ -28,273 +27,33 @@ const router = createBrowserRouter([
       </SignedIn>
     ),
     children: [
-      {
-        path: "/",
-        element: (
-          <SignedIn>
-            <Home />
-          </SignedIn>
-        ),
-      },
-      {
-        path: "/restaurant/:id",
-        element: (
-          <SignedIn>
-            <Restaurant />
-          </SignedIn>
-        ),
-      },
-      {
-        path: "/contact",
-        element: (
-          <SignedIn>
-            <Contact />
-          </SignedIn>
-        ),
-      },
-      {
-        path: "/terms-and-conditions",
-        element: (
-          <SignedIn>
-            <TermsAndConditions />
-          </SignedIn>
-        ),
-      },
-      {
-        path: "/privacy-policy",
-        element: (
-          <SignedIn>
-            <PrivacyPolicy />
-          </SignedIn>
-        ),
-      },
-      {
-        path: "/career",
-        element: (
-          <SignedIn>
-            <Career />
-          </SignedIn>
-        ),
-      },
-      {
-        path: "/for-customers",
-        element: (
-          <SignedIn>
-            <ForCustomers />
-          </SignedIn>
-        ),
-      },
-      {
-        path: "/for-delivery-staff",
-        element: (
-          <SignedIn>
-            <ForDeliveryStaff />
-          </SignedIn>
-        ),
-      },
-      {
-        path: "/profile",
-        element: (
-          <SignedIn>
-            <Profile />
-          </SignedIn>
-        ),
-      },
-      {
-        path: "/checkout",
-        element: (
-          <SignedIn>
-            <Checkout />
-          </SignedIn>
-        ),
-      },
-      {
-        path: "/order",
-        element: (
-          <SignedIn>
-            <Order />
-          </SignedIn>
-        ),
-      },
+      { path: "/", element: <Home /> },
+      { path: "/restaurant/:id", element: <Restaurant /> },
+      { path: "/contact", element: <Contact /> },
+      { path: "/terms-and-conditions", element: <TermsAndConditions /> },
+      { path: "/privacy-policy", element: <PrivacyPolicy /> },
+      { path: "/career", element: <Career /> },
+      { path: "/for-customers", element: <ForCustomers /> },
+      { path: "/for-delivery-staff", element: <ForDeliveryStaff /> },
+      { path: "/profile", element: <Profile /> },
+      { path: "/checkout", element: <Checkout /> },
+      { path: "/order", element: <Order /> },
     ],
   },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
+  { path: "/signup", element: <Signup /> },
+  { path: "/login", element: <Login /> },
 ]);
 
 function App() {
   return (
-    <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
+    <>
       <SignedOut>
         <RedirectToSignIn />
       </SignedOut>
       <RouterProvider router={router} />
-    </ClerkProvider>
+    </>
   );
 }
 
 export default App;
 
-/*import {
-  createBrowserRouter,
-  RouterProvider,
-  useNavigate,
-} from "react-router-dom";
-import AppLayout from "./ui/AppLayout";
-import Home from "./pages/home/Home";
-import CartModal from "./ui/Cart";
-import Restaurant from "./pages/restaurant/Restaurant";
-import { useEffect, useState } from "react";
-import Contact from "./pages/footer pages/contact/Contact";
-import TermsAndConditions from "./pages/footer pages/terms-and-conditions/TermsAndConditions";
-import PrivacyPolicy from "./pages/footer pages/privacy-policy/PrivacyPolicy";
-import Career from "./pages/footer pages/career/Career";
-import ForCustomers from "./pages/footer pages/for-customers/ForCustomers";
-import ForDeliveryStaff from "./pages/footer pages/for-delivery-staff/ForDeliveryStaff";
-import Signup from "./pages/signup/Signup";
-import Profile from "./pages/profile/Profile";
-import Checkout from "./pages/checkout/Checkout";
-import Login from "./pages/login/Login";
-import { AuthContext, AuthProvider } from "./auth/AuthContext";
-import ProtectedRoute from "./auth/ProtectedRoute";
-import Order from "./pages/order/Order";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <ProtectedRoute>
-        <AppLayout />
-      </ProtectedRoute>
-    ),
-    children: [
-      {
-        path: "/",
-        element: (
-          <ProtectedRoute>
-            <Home />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "/restaurant/:id",
-        element: (
-          <ProtectedRoute>
-            <Restaurant />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "/contact",
-        element: (
-          <ProtectedRoute>
-            <Contact />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "/terms-and-conditions",
-        element: (
-          <ProtectedRoute>
-            <TermsAndConditions />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "/privacy-policy",
-        element: (
-          <ProtectedRoute>
-            <PrivacyPolicy />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "/career",
-        element: (
-          <ProtectedRoute>
-            <Career />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "/for-customers",
-        element: (
-          <ProtectedRoute>
-            <ForCustomers />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "/for-delivery-staff",
-        element: (
-          <ProtectedRoute>
-            <ForDeliveryStaff />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "/profile",
-        element: (
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "/checkout",
-        element: (
-          <ProtectedRoute>
-            <Checkout />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "/order",
-        element: (
-          <ProtectedRoute>
-            <Order />
-          </ProtectedRoute>
-        ),
-      },
-    ],
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-]);
-
-function App() {
-  const [authState, setAuthState] = useState(null);
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      setAuthState(true);
-    } else {
-      setAuthState(false);
-    }
-  }, []);
-
-  if (authState === null) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <AuthProvider value={{ authState, setAuthState }}>
-      <RouterProvider router={router} />
-    </AuthProvider>
-  );
-}
-
-export default App;*/
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,9 @@
-/*import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-hgohsdoug
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)*/
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ClerkProvider } from "@clerk/clerk-react"; // Clerk importálása
 
-// Clerk Publishable Key importálása
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
